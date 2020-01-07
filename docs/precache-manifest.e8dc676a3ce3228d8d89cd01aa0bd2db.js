c86c4b1d13aa99b31191e1ded91c88c4185986f7
andoshin11 <shinglish11@gmail.com>
2020-01-07 22:38:58 +0900

    build artifact

---
+++ docs/precache-manifest.e8dc676a3ce3228d8d89cd01aa0bd2db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "189295f39ad25c3c7e05",
+    "url": "/css/advanced.fa3114b6.css"
+  },
+  {
+    "revision": "189295f39ad25c3c7e05",
+    "url": "/js/advanced.29c7d1c3.js"
+  },
+  {
+    "revision": "45bd7c16365f8bda3772",
+    "url": "/css/app.6f317696.css"
+  },
+  {
+    "revision": "45bd7c16365f8bda3772",
+    "url": "/js/app.7833a889.js"
+  },
+  {
+    "revision": "8c34364340e0bf18fb9b",
+    "url": "/js/chunk-vendors.ae880145.js"
+  },
+  {
+    "revision": "572035ca5c7cb4794c527a88907d29f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
