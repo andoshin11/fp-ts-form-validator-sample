27ba32a6c49de4b1338d6828a6685b535df8367a
andoshin11 <shinglish11@gmail.com>
2020-01-07 13:43:54 +0900

    Add PrettyJson component

---
+++ src/components/PrettyJson.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+
+interface Props {
+  json: object
+}
+
+export default Vue.extend<Props>({
+  functional: true,
+  name: 'PrettyJson',
+  props: {
+    json: {
+      type: Object,
+      required: true
+    }
+  },
+  render(h, ctx) {
+    const { props } = ctx
+    const stringified = JSON.stringify(props.json, null, 2)
+    return h('pre', stringified)
+  }
+})
